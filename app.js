5c6e8463352dadaff98b2bdf8ab936b05e890116
megastar98 <obikoya11@gmail.com>
2017-09-20 09:53:59 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.get('/',function(req,res,next){
+    
+})
+
+
+
+app.listen(3000,function(){
+    console.log('server started running on port 3000');
+})
